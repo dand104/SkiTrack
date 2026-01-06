6877f9bca8cd1a5305bf1918d515532c8baef179
danil <danil.kukuev@gmail.com>
2026-01-06 14:45:51 +0500

    fix style url

--- app/src/main/kotlin/org/skitrace/skitrace/map/MapStyles.kt
+++ app/src/main/kotlin/org/skitrace/skitrace/map/MapStyles.kt
@@ -2,7 +2,7 @@ package org.skitrace.skitrace.map
 
 object MapStyles {
     const val STYLE_LIBERTY = "https://tiles.openfreemap.org/styles/liberty"
-    const val STYLE_DARK = "https://tiles.openfreemap.org/styles/dark-matter"
+    const val STYLE_DARK = "https://tiles.openfreemap.org/styles/fiord"
 
     const val TILE_PISTES_URL = "https://tiles.opensnowmap.org/pistes/{z}/{x}/{y}.png"
     const val ATTRIBUTION_OPENSNOW = "<a href=\"https://www.opensnowmap.org\">© OpenSnowMap</a>"
